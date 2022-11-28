e0f148f67cd3efdd500ded298b82ffe9997ed65b
Barashov <BarashovMisha@gmail.com>
2022-11-28 20:32:07 +0300

    creating api doc

--- users/views.py
+++ users/views.py
@@ -1,22 +1,17 @@
+from drf_yasg import openapi
 from rest_framework.decorators import api_view
+from .doc import *
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import *
 from .logics.registration import is_username_taken, is_email_taken
 from rest_framework.parsers import MultiPartParser, JSONParser
 from rest_framework.permissions import IsAuthenticated
+from drf_yasg.utils import swagger_auto_schema
 
 
 class UserAPIView(APIView):
-    def get(self, request):
-        user = request.user
-        if user.is_authenticated:
-            serializer = ProfileSerializer(user)
-
-            return Response(status=200, data=serializer.data)
-
-        return Response(status=403)
-
+    @swagger_auto_schema(**user_create_doc)
     def post(self, request):
         serializer = RegistrationSerializer(data=request.data)
         if serializer.is_valid():
@@ -29,6 +24,7 @@ class UserAPIView(APIView):
 
 
 class LoginAPIView(APIView):
+    @swagger_auto_schema(**user_login_doc)
     def post(self, request):
         serializer = LoginSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
@@ -37,14 +33,16 @@ class LoginAPIView(APIView):
 
 
 class ProfileAPIView(APIView):
-    parser_classes = (MultiPartParser, JSONParser)
+    parser_classes = (MultiPartParser, )
     permission_classes = (IsAuthenticated, )
 
+    @swagger_auto_schema(**get_profile_doc)
     def get(self, request):
         user = request.user
         serializer = ProfileSerializer(user)
         return Response(status=200, data=serializer.data)
 
+    @swagger_auto_schema(**update_profile_doc)
     def patch(self, request):
         serializer = ProfileSerializer(instance=request.user,
                                        data=request.data,
@@ -55,6 +53,7 @@ class ProfileAPIView(APIView):
         return Response(400)
 
 
+@swagger_auto_schema(method="get", **is_authenticated_doc)
 @api_view()
 def is_authenticated_view(request):
     user = request.user
@@ -65,6 +64,7 @@ def is_authenticated_view(request):
     return Response(status=200, data=response)
 
 
+@swagger_auto_schema(method='get', **is_username_taken_doc)
 @api_view()
 def is_username_taken_view(request, username):
     response = {
@@ -74,6 +74,7 @@ def is_username_taken_view(request, username):
     return Response(status=200, data=response)
 
 
+@swagger_auto_schema(method='get', **is_email_taken_doc)
 @api_view()
 def is_email_taken_view(request, email):
     response = {
